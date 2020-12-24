bd0cef5de1e10b0395e82217077d35fe11c80a6d
Antonio Berrones <antoniob@mit.edu>
2020-12-24 00:25:10 -0600

    add to constant.py

--- spotify/constant.py
+++ spotify/constant.py
@@ -1,4 +1,11 @@
 import os
 
+# define endpoint urls
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+
+# define response status codes
+STATUS_OK = 200
+STATUS_CREATED = 201
+
 CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("SPOTIFY_SECRET")
