3c0320511f20ff8e4723fe7f0dc6dc2343921677
Antonio Berrones <antoniob@mit.edu>
2020-12-23 23:51:35 -0600

    create clientCredentialFlow.py

---
+++ spotify/clientCredentialFlow.py
@@ -0,0 +1,28 @@
+class ClientCredentialFlow(SpotifyClient):
+    """
+    Represents a SpotifyClient that uses the Client Credential Flow for authorization.
+    Can only access endpoints that do not access user information.
+
+    More info at: https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
+    """
+
+    def __init__(self, clientId: str, clientSecret: str):
+        """
+        Creates an instance of a ClientCredentialFlow SpotifyClient.
+
+        Args:
+            clientId (str): The client id.
+            clientSecret (str): The client secret key.
+        """
+        self.clientId = clientId
+        self.clientSecret = clientSecret
+        self.grantType = "client_credentials"
+
+    def getToken(self) -> str:
+        """
+        Gets an authorization token.
+
+        Returns:
+            str: A valid authorization token.
+        """
+        pass
