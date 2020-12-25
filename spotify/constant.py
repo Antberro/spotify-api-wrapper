e6c5dfd06b6863ab83b068dbfce1e5ef6fbe7d8f
Antonio Berrones <antoniob@mit.edu>
2020-12-24 19:57:49 -0600

    add to constant.py

--- spotify/constant.py
+++ spotify/constant.py
@@ -8,5 +8,6 @@ BASE_URL = "https://api.spotify.com/v1"
 STATUS_OK = 200
 STATUS_CREATED = 201
 
+# FOR DEVELOPMENT
 CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("SPOTIFY_SECRET")
