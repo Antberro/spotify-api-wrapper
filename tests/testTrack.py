3322db62a76d0477eb89b4f350831f7a6d2df2d8
Antonio Berrones <antoniob@mit.edu>
2020-12-24 20:24:33 -0600

    write tests for getSeveralTracks()

--- tests/testTrack.py
+++ tests/testTrack.py
@@ -37,3 +37,45 @@ class TestGetTrack(unittest.TestCase):
     # covers: authorization code flow, response is invalid
     def test_invalid_auth_code(self):
         pass #TODO
+
+
+class TestGetSeveralTracks(unittest.TestCase):
+    """
+    Testing Strategy
+
+    Partition on authorization flow:
+        - client credential flow
+        - authorization code
+
+    Partition on response:
+        - response is valid
+        - response is invalid
+    """
+        # covers: client credential flow, response is valid
+    def test_valid_client_cred(self):
+        client = SpotifyClient.usingClientCredential(CLIENT_ID, CLIENT_SECRET)
+        trackIds = [
+            "1LeWIs2hP2r5yOQnVuYoI5",
+            "0qdQUeKVyevrbKhAo0ibxS",
+            "6Ac4NVYYl2U73QiTt11ZKd"
+        ]
+        client.track.getSeveralTracks(trackIds)
+
+    # covers: client credential flow, response is invalid
+    def test_invalid_client_cred(self):
+        client = SpotifyClient.usingClientCredential(CLIENT_ID, CLIENT_SECRET)
+        trackIds = [
+            "invalid id1",
+            "invalid id2",
+            "invalid id3" 
+        ]
+        with self.assertRaises(HTTPError): 
+            client.track.getSeveralTracks(trackIds)
+
+    # covers: authorization code flow, response is valid
+    def test_valid_auth_code(self):
+        pass #TODO
+
+    # covers: authorization code flow, response is invalid
+    def test_invalid_auth_code(self):
+        pass #TODO
