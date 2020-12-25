db0cbadc07b788b9f6aa4adf7d0fdaf1f4b4554e
Antonio Berrones <antoniob@mit.edu>
2020-12-25 15:40:01 -0600

    write specs for methods in AlbumManager

--- spotify/albumManager.py
+++ spotify/albumManager.py
@@ -15,11 +15,43 @@ class AlbumManager(object):
         """
         self.client = client
 
-    def getAlbum(self) -> dict:
+    def getAlbum(self, albumId: str, market: str = None) -> dict:
+        """
+        Get the album with the given Spotify id.
+
+        Args:
+            albumId (str): The Spotify id for the album.
+            market (str, optional): A country code or "from_token"; used for Track Relinking. Defaults to None.
+
+        Returns:
+            dict: response from Spotify Web API, a album object in json format
+        """
         pass
 
-    def getSeveralAlbums(self) -> dict:
+    def getSeveralAlbums(self, albumIds: list, market: str = None) -> dict:
+        """
+        Get multiple albums with the given Spotify ids. Maximum of 20 ids.
+
+        Args:
+            albumIds (list): The list of Spotify album ids.
+            market (str, optional): A country code or "from_token"; used for Track Relinking. Defaults to None.
+
+        Returns:
+            dict: response from Spotify Web API, a collection of album objects in json format
+        """
         pass
 
-    def getTracksOfAlbum(self) -> dict:
+    def getTracksOfAlbum(self, albumId: str, limit: int = 20, offset: int = 0, market: str = None) -> dict:
+        """
+        Get the tracks of the album with the given albumId.
+
+        Args:
+            albumId (str): The Spotify id for the album.
+            limit (int, optional): The maximum number of tracks to return. Range from [1, 50]. Defaults to 20.
+            offset (int, optional): The index of the first track to return. Defaults to 0.
+            market (str, optional): A country code or "from_token"; used for Track Relinking. Defaults to None.
+
+        Returns:
+            dict: response from Spotify Web API, a collection of track objects wrapped in a paging object in json format
+        """
         pass
