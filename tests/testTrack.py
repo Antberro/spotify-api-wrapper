f8f6543ac20cf9a71c6d696eb5e2efc62b9d429c
Antonio Berrones <antoniob@mit.edu>
2020-12-24 20:52:00 -0600

    write tests for getAudioFeatures()

--- tests/testTrack.py
+++ tests/testTrack.py
@@ -79,3 +79,37 @@ class TestGetSeveralTracks(unittest.TestCase):
     # covers: authorization code flow, response is invalid
     def test_invalid_auth_code(self):
         pass #TODO
+
+
+class TestGetAudioFeatures(unittest.TestCase):
+    """
+    Testing Strategy
+
+    Partition on authorization flow:
+        - client credential flow
+        - authorization code
+
+    Partition on response:
+        - response is valid
+        - response is invalid
+    """
+    # covers: client credential flow, response is valid
+    def test_valid_client_cred(self):
+        client = SpotifyClient.usingClientCredential(CLIENT_ID, CLIENT_SECRET)
+        trackId = "1LeWIs2hP2r5yOQnVuYoI5"
+        client.track.getAudioFeatures(trackId)
+
+    # covers: client credential flow, response is invalid
+    def test_invalid_client_cred(self):
+        client = SpotifyClient.usingClientCredential(CLIENT_ID, CLIENT_SECRET)
+        trackId = "1LeWIs2hP2r5yOQnVuYoI5"
+        with self.assertRaises(HTTPError): 
+            client.track.getAudioFeatures(trackId)
+
+    # covers: authorization code flow, response is valid
+    def test_valid_auth_code(self):
+        pass #TODO
+
+    # covers: authorization code flow, response is invalid
+    def test_invalid_auth_code(self):
+        pass #TODO
