09e75eb5c7f167dcc0402ccfc3ce9781435cee0b
Antonio Berrones <antoniob@mit.edu>
2021-01-03 17:27:14 -0600

    implement addTrack()

--- spotify/playlistManager.py
+++ spotify/playlistManager.py
@@ -182,6 +182,8 @@ class PlaylistManager(object):
         """
         Add one or more tracks to a Spotify user's playlist.
 
+        Requires scope: "playlist-modify-public" or "playlist-modify-private"
+
         Args:
             playlistId (str): The Spotify id for the playlist.
             uris (list): List of Spotify uris to add. Maximum of 100.
@@ -190,7 +192,20 @@ class PlaylistManager(object):
         Returns:
             dict: response from Spotify Web API, a snapshot id in json format; used to identify the playlist version in future requests
         """
-        pass
+        
+        # define param and header args for request
+        url = BASE_URL + "/playlists/" + playlistId + "/tracks"
+        headers = {
+            "Authorization": "Bearer " + self.client.getCurrentToken(),
+            "Content-Type": "application/json"
+            }
+        params = {"uris": uris}
+        if position:
+            params["position"] = position
+
+        # send request
+        response = self.client._sendHTTPRequest("POST", url, params, headers)
+        return response
 
     # Remove Items from a Playlist
     def removeTrack(self, playlistId: str, tracks: list, positions: list = None , snapshotId: str = None) -> dict:
